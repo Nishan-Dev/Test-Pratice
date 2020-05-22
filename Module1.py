1ac6d1bf0fd6f01e3386402f075e297a3f0c1a78
nishanpahadi <nishanpahadi7@users.noreply.github.com>
2020-05-22 14:59:35 -0500

    pushing project

---
+++ Module1.py
@@ -0,0 +1,34 @@
+class CalcPerson:
+
+
+
+    def subtracting(self):
+        vars = int(input("enter a number 1: "))
+        vars1 = int(input("enter a number 2: "))
+        x = vars - vars1
+        print(x)
+        return
+
+
+    def adding(self):
+        vars = int(input("enter a number 1: "))
+        vars1 = int(input("enter a number 2: "))
+        x = vars + vars1
+        print(x)
+        return
+
+
+    def multiplication(self):
+        vars = int(input("enter a number 1: "))
+        vars1 = int(input("enter a number 2: "))
+        x = vars * vars1
+        print(x)
+        return
+
+    def division(self):
+        vars = int(input("enter a number 1: "))
+        vars1 = int(input("enter a number 2: "))
+        x = vars / vars1
+        print(x)
+        return
+
